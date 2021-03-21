b600cb20c2da91357dd471d34be48cc4143d8c7e
Wilson Javier <wjalmarior@unal.edu.co>
2021-03-21 12:15:26 -0500

    update

--- CNN/02_hardware/00_test_cpp/test.cpp
+++ CNN/02_hardware/00_test_cpp/test.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-// gcc -o test test.cpp -lm
+// g++ -o test test.cpp -lm
 
 void    QuantizeMultiplier(double double_multiplier, int32_t* quantized_multiplier, int* shift);
 
@@ -17,19 +17,31 @@ int main ()
   double param, result;
   int n;
 
-  param = 8.0;
-  result = frexp (param , &n);
-  printf ("%f = %f * 2^%d\n", param, result, n);
-  
+  int* shift;
+  int32_t* M0;
+  float scale = 0.04698891192674637;
+  double M = 0.0001722/scale;
+
+  //param = 8.0;
+  //result = frexp (param , &n);
+  //printf ("%f = %f * 2^%d\n", param, result, n);
+  cout << "--- QuantizeMultiplier ---" << endl;
+  QuantizeMultiplier(M,M0,shift);
+  cout << "M = "     << M << std::endl;
+  cout << "M0 = " << M0 << std::endl;
+  cout << "shift = " << shift << std::endl;
+
+
 }
 
 
 
 void QuantizeMultiplier(double double_multiplier, int32_t* quantized_multiplier, int* shift){
   if (double_multiplier == 0.) {
-    *quantized_multiplier = 0; *shift = 0; return; } 
-
+    *quantized_multiplier = 0; *shift = 0; return; }
+  cout << "--- QuantizeMultiplier 2 ---" << endl;
   const double q       = frexp(double_multiplier, shift);
+        cout << "--- QuantizeMultiplier 3---" << endl;
         double q_fixed = static_cast<int64_t>(round(q*(1ll << 31)));
 
   if (q_fixed == (1ll << 31)) {
@@ -41,9 +53,9 @@ void QuantizeMultiplier(double double_multiplier, int32_t* quantized_multiplier,
   *quantized_multiplier = static_cast<int32_t>(q_fixed);
 
   /*std::cout << "--- Résultats en CPP ---" << std::endl;
-  std::cout << "double multiplier = "     << double_multiplier     << std::endl; 
-  std::cout << "q = "                     << q                     << std::endl; 
-  std::cout << "q_fixed = "               << q_fixed               << std::endl; 
-  std::cout << "*shift  = "               << *shift                << std::endl; 
+  std::cout << "double multiplier = "     << double_multiplier     << std::endl;
+  std::cout << "q = "                     << q                     << std::endl;
+  std::cout << "q_fixed = "               << q_fixed               << std::endl;
+  std::cout << "*shift  = "               << *shift                << std::endl;
   std::cout << "quantized_multiplier = "  << *quantized_multiplier << std::endl;  */
 }

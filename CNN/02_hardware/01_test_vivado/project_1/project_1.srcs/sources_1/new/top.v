0067a9d584603ac317c279ec4feed5d679a9375d
wjalmarior <wjalmarior@unal.edu.co>
2021-03-31 02:46:36 -0500

    module for quantization

--- CNN/02_hardware/01_test_vivado/project_1/project_1.srcs/sources_1/new/top.v
+++ CNN/02_hardware/01_test_vivado/project_1/project_1.srcs/sources_1/new/top.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Universidad Nacional de Colombia
+// Engineer: Wilson Javier Almario R.
 // 
-// Create Date: 12/09/2019 09:19:03 AM
-// Design Name: 
+// Create Date: 31/03/2021
+// Design Name: quantization
 // Module Name: top
-// Project Name: 
-// Target Devices: 
+// Project Name: Quantized CNN implementation on FPGA 
+// Target Devices: Zybo Z7010
 // Tool Versions: 
 // Description: 
 // 
@@ -19,17 +19,16 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module top
 #(
 	parameter s0 = 4'b0000, s1 = 4'b0001, s2 = 4'b0010, s3 = 4'b0011, s4 = 4'b0100,
     parameter s5 = 4'b0101, s6 = 4'b0110, s7 = 4'b0111, s8 = 4'b1000, s9 = 4'b1001,
-	//A = 2'b11,
-	B = 2'b01,
-	C = 2'b10,
-	//q = 31'b1111000000101011010111100110000
-	q = 64'd2014687024,
-	mask = 8'd255
+    parameter s10 = 4'b1010, s11 = 4'b1011, s12 = 4'b1100, s13 = 4'b1101, s14 = 4'b1110,
+	
+	q = 64'd2014687024, //q = 31'b1111000000101011010111100110000
+	mask = 8'd255,
+	zero = 1'd0,
+	one = 1'd1
 )
 ( input clk,
   input en,
@@ -43,21 +42,23 @@ module top
     
     reg [64:0] result1;
     reg [31:0] result2;
-    reg [8:0] result3;
-    reg [31:0] threshold;
+    reg [8:0]  result3;
+    reg [8:0]  result4;
+    reg [31:0] remainder;
+    reg [8:0] thld1;
+    reg thld2;
+    reg thld3; 
+    reg [8:0] threshold;
+    reg [8:0] res1;
+    reg [8:0] res2;
+    reg res3;
+    reg [8:0] res4;  
     
-      
+//    clock_divider clock_1s(
 
-    
-    
-    
-    clock_divider clock_1s(
-
-        .clk(clk),
-        .divided_clk(clk_1s)
-    );
-    
-    
+//        .clk(clk),
+//        .divided_clk(clk_1s)
+//    );
     always @(posedge clk) //Present estate 
     begin
         if(en && flag == 1)
@@ -97,11 +98,13 @@ module top
             s7:
                 next_state <= s8;  
             s8:
-                next_state <= s9;                                                        
+                next_state <= s9;
+            s9:
+                next_state <= s10;
+            s10:
+                next_state <= s11;                                                                                         
         endcase                
     end
-
- 
     //===================    
     // Output logic
     //===================     
@@ -112,22 +115,32 @@ module top
         s1:
           result2 <= result1 >>> 31;
         s2:
-          result3 <= result2 >>> 8;
+          remainder <= result2 & mask;
         s3:
-          threshold <= result2 & mask;
+          result3 <= result2 >>> 8;          
         s4:
-          result <= 4'b0100;
+          thld1 <= mask >>> 1;//ShiftRight(mask, 1)            
         s5:
-          result <= 4'b0101;
+          thld2 <= ($signed(result3) < $signed(zero)); //MaskIfLessThan(x, zero)
         s6:
-          result <= 4'b0110;
+          thld3 <= thld2 & 1; //BitAnd(MaskIfLessThan(x, zero), one))
         s7:
-          result <= 4'b0111;
+          threshold <= thld1 + thld2; //Add(ShiftRight(mask, 1), BitAnd(MaskIfLessThan(x, zero), one));
         s8:
-          result <= 4'b1000;
-        s9: 
-            result <= 4'b1001;
-           
+          res1 <= result2 >>> 8; //ShiftRight(x, exponent)
+        s9:
+          if($signed(remainder) > $signed(threshold))
+          begin
+            res2 <= -1'd1;
+          end
+          else begin
+            res2 <= -1'd0;
+          end   
+          //res2 <= ($signed(remainder) > $signed(threshold)); //MaskIfGreaterThan(remainder, threshold)
+        s10:
+          res3 <= res2 & one; //BitAnd( MaskIfGreaterThan(remainder, threshold), one ) 
+        s11:
+          result4 <= res1 + res3; //Add( ShiftRight(x, exponent),BitAnd( MaskIfGreaterThan(remainder, threshold), one ) );                   
       endcase 
     end 
 

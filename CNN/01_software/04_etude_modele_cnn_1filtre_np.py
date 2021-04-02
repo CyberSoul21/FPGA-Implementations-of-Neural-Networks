88fe17935b9d1cc7e0902d8108d71fc9c888d6fb
wjalmarior <wjalmarior@unal.edu.co>
2021-04-01 23:19:34 -0500

    image storage into FPGA

--- CNN/01_software/04_etude_modele_cnn_1filtre_np.py
+++ CNN/01_software/04_etude_modele_cnn_1filtre_np.py
@@ -107,7 +107,7 @@ print(img_test);
 count = 0
 for i in range(0,28):
   for j in range(0,28):
-    print("data["+str(count)+"] = "+"8'd"+str(img_test[i][j])+";")
+    print("data["+str(count)+"] = "+"7'd"+str(img_test[i][j])+";")
     count +=1 
 
 print(" \n ");
